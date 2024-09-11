814045ea37f485ca8efb0693aa402f3a3885dcc9
Vlad123abc <vladi.ivanescu@gmail.com>
2024-09-11 19:14:00 +0300

    implemented logout ClientWorker tests. they pass

--- networking/src/test/java/ClientWorkerTest.java
+++ networking/src/test/java/ClientWorkerTest.java
@@ -102,4 +102,50 @@ public class ClientWorkerTest {
         // assert that the exception was transformed to reject
         assertEquals(responseData.getType(), ResponseType.ERROR);
     }
+
+    @Test
+    public void logoutTest() throws Exception
+    {
+        // setting up input that will be sent
+        String r = "{'type'='LOGOUT', 'data'={'username'='vlad', 'password'='parola', 'id'=0}}" + System.lineSeparator();
+        // setting up input reader - just as we were reading from the socket
+        StringReader sr = new StringReader(r);
+        var input = new BufferedReader(sr);
+
+        // setting up writer - our ClientWorker will write responses here. We will assert later what the test output was.
+        StringWriter sw = new StringWriter();
+        var output = new PrintWriter(sw);
+
+        // Mock objects - these implement interfaces, and they do nothing yet.
+        IService mockService = Mockito.mock(IService.class);
+        Closeable mockSocket = Mockito.mock(Closeable.class);
+        // create the worker with input, output and the mock objects - nothing is real here, we test the CW in isolation.
+        ClientWorker cw = new ClientWorker(mockService, mockSocket, input, output);
+        Thread t = new Thread(cw);
+        t.start();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception ignored) {}
+        cw.stop();
+        try {
+            t.join();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Mockito.verify(mockService).logout(Mockito.any(), Mockito.any());
+
+        // now we assert the output
+        // we read line by line from the response object
+        String response = new String(sw.getBuffer());
+
+        StringReader responseReader = new StringReader(response);
+        var responseInput = new BufferedReader(responseReader);
+        var responseLine = responseInput.readLine();
+        Gson gson = new Gson();
+        // was the logout response ok?
+        var responseData = gson.fromJson(responseLine, Response.class);
+        assertEquals(responseData.getType(), ResponseType.OK);
+    }
 }
