a9e42e8bcc97758ae7c09e614d8fa7cf161e448e
Vlad123abc <vladi.ivanescu@gmail.com>
2024-09-11 18:59:31 +0300

    implemented login ClientWorker tests. they pass

--- networking/src/main/java/deposit/networking/jsonProtocol/ClientWorker.java
+++ networking/src/main/java/deposit/networking/jsonProtocol/ClientWorker.java
@@ -10,7 +10,6 @@ import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 public class ClientWorker implements Runnable, IObserver {
     private IService server;
---
+++ networking/src/test/java/ClientWorkerTest.java
@@ -0,0 +1,105 @@
+import com.google.gson.Gson;
+import deposit.networking.jsonProtocol.ClientWorker;
+import deposit.networking.jsonProtocol.Response;
+import deposit.networking.jsonProtocol.ResponseType;
+import deposit.service.IService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientWorkerTest {
+    @Test
+    public void loginOk() throws Exception
+    {
+        // setting up input that will be sent
+        String r = "{'type'='LOGIN', 'data'={'username'='vlad', 'password'='parola', 'id'=0}}" + System.lineSeparator();
+        // setting up input reader - just as we were reading from the socket
+        StringReader sr = new StringReader(r);
+        var inputBufferedReader = new BufferedReader(sr);
+
+        // setting up writer - our ClientWorker will write responses here. We will assert later what the test output was.
+        StringWriter outputWriter = new StringWriter();
+        var outputPrinter = new PrintWriter(outputWriter);
+
+        // Mock objects - these implement interfaces, and they do nothing yet.
+        IService mockService = Mockito.mock(IService.class);
+        Closeable mockSocket = Mockito.mock(Closeable.class);
+        // create the worker with input, output and the mock objects - nothing is real here, we test the CW in isolation.
+        ClientWorker cw = new ClientWorker(mockService, mockSocket, inputBufferedReader, outputPrinter);
+        Thread t = new Thread(cw);
+        // rulam clientworker care va face pasii 2,3,4,5
+        t.start();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception ignored) {}
+        cw.stop();
+        try {
+            t.join();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // verificare pas 3 (s-a apelat login pe service?)
+        Mockito.verify(mockService).login(Mockito.eq("vlad"), Mockito.eq("parola"),Mockito.any());
+
+        // pas 6.
+        // now we assert the output
+        // we read line by line from the response object
+        String allResponsesOneResponsePerLine = new String(outputWriter.getBuffer());
+
+        StringReader responseReader = new StringReader(allResponsesOneResponsePerLine);
+        var responseInput = new BufferedReader(responseReader);
+        var responseLine = responseInput.readLine();
+        Gson gson = new Gson();
+        // was the login response ok?
+        var responseData = gson.fromJson(responseLine, Response.class);
+        assertEquals(responseData.getType(), ResponseType.OK);
+    }
+
+    @Test
+    public void loginFail() throws Exception
+    {
+        // input
+        String r = "{'type'='LOGIN', 'data'={'username'='vladaaa', 'password'='parola', 'id'=0}}" + System.lineSeparator();
+        // setting up stuff
+        StringReader sr = new StringReader(r);
+        StringWriter sw = new StringWriter();
+        var input = new BufferedReader(sr);
+        var output = new PrintWriter(sw);
+        IService mockService = Mockito.mock(IService.class);
+
+        // now we say login will be throw an error from our mock service
+        Mockito.doThrow(new Exception()).when(mockService).login(Mockito.eq("vladaaa"), Mockito.eq("parola"),Mockito.any());
+        Closeable mockSocket = Mockito.mock(Closeable.class);
+        ClientWorker cw = new ClientWorker(mockService, mockSocket, input, output);
+        Thread t = new Thread(cw);
+        t.start();
+        try {
+            Thread.sleep(1000);
+        }
+        catch (Exception ignored) {}
+        cw.stop();
+        try {
+            t.join();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Mockito.verify(mockService).login(Mockito.eq("vladaaa"), Mockito.eq("parola"),Mockito.any());
+
+        String response = new String(sw.getBuffer());
+
+        StringReader responseReader = new StringReader(response);
+        var responseInput = new BufferedReader(responseReader);
+        var responseLine = responseInput.readLine();
+        Gson gson = new Gson();
+        var responseData = gson.fromJson(responseLine, Response.class);
+        // assert that the exception was transformed to reject
+        assertEquals(responseData.getType(), ResponseType.ERROR);
+    }
+}
