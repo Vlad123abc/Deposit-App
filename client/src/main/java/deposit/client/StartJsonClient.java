0ddaba2455849a74bdaf8793195de385bd67bad8
Vlad123abc <vladi.ivanescu@gmail.com>
2024-09-12 12:38:10 +0300

    frontend work

--- client/src/main/java/deposit/client/StartJsonClient.java
+++ client/src/main/java/deposit/client/StartJsonClient.java
@@ -1,11 +1,69 @@
 package deposit.client;
 
+import deposit.client.gui.LoginController;
+import deposit.client.gui.UserController;
+import deposit.networking.jsonProtocol.ServiceProxy;
+import deposit.service.IService;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Properties;
+
 public class StartJsonClient extends Application {
+    private static String defaultServer = "localhost";
+    private static int defaultChatPort = 55556;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("In start");
 
+        Properties clientProps = new Properties();
+        try {
+            clientProps.load(StartJsonClient.class.getResourceAsStream("/client.props"));
+            System.out.println("Client properties set.");
+            clientProps.list(System.out);
+        }
+        catch (IOException e) {
+            System.err.println("Cannot find transportclient.properties " + e);
+            return;
+        }
+
+        String serverIP = clientProps.getProperty("deposit.server.host", defaultServer);
+        int serverPort = defaultChatPort;
+        try {
+            serverPort = Integer.parseInt(clientProps.getProperty("deposit.server.port"));
+        }
+        catch (NumberFormatException ex) {
+            System.err.println("Wrong port number " + ex.getMessage());
+            System.out.println("Using default port: " + defaultChatPort);
+        }
+        System.out.println("Using server IP " + serverIP);
+        System.out.println("Using server port " + serverPort);
+
+        Socket socket = new Socket(serverIP, serverPort);
+        var output = new PrintWriter((socket).getOutputStream());
+        var input = new BufferedReader(new InputStreamReader((socket).getInputStream()));
+        IService server = new ServiceProxy(socket, input, output);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+
+        LoginController loginController = fxmlLoader.getController();
+        loginController.initController(server);
+
+        FXMLLoader fxmlLoaderUser = new FXMLLoader(getClass().getClassLoader().getResource("user-view.fxml"));
+        UserController userController = fxmlLoaderUser.getController();
+        loginController.setUserController(userController);
+
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
--- client/src/main/java/deposit/client/gui/LoginController.java
+++ client/src/main/java/deposit/client/gui/LoginController.java
@@ -1,7 +1,67 @@
 package deposit.client.gui;
 
+import deposit.domain.User;
 import deposit.service.IService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
     private IService service;
+    private UserController userController;
+
+    @FXML
+    private TextField textField;
+    @FXML
+    private PasswordField passwordField;
+
+    public void initController(IService service) {
+        this.service = service;
+    }
+
+    public void setUserController(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void onLogin(ActionEvent actionEvent) throws Exception
+    {
+        String username = this.textField.getText();
+        String password = this.passwordField.getText();
+
+        if (username.isEmpty())
+            MessageWindow.showMessage(null, Alert.AlertType.ERROR, "Error", "Completati Username!");
+        if (password.isEmpty())
+            MessageWindow.showMessage(null, Alert.AlertType.ERROR, "Error", "Completati Password!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("user-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 800);
+
+        this.userController = fxmlLoader.getController();
+
+        try
+        {
+            this.service.login(username, password, this.userController);
+
+            this.textField.clear();
+            this.passwordField.clear();
+
+            //TODO
+//            User user = this.service.getUserByUsername(username);
+//            this.userController.init_controller(this.service, user);
+            Stage stage = new Stage();
+            stage.setTitle(username);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e)
+        {
+            MessageWindow.showMessage(null, Alert.AlertType.ERROR, "Autentication Failed!", "Wrong username or password!");
+            this.passwordField.clear();
+        }
+    }
 }
