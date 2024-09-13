fd1ab257def4a6b348bc0bdf2e7408636d9dafc4
Vlad123abc <vladi.ivanescu@gmail.com>
2024-09-13 10:52:25 +0300

    implemented logout gui

--- client/src/main/java/deposit/client/gui/UserController.java
+++ client/src/main/java/deposit/client/gui/UserController.java
@@ -4,10 +4,17 @@ import deposit.domain.Package;
 import deposit.domain.User;
 import deposit.service.IObserver;
 import deposit.service.IService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class UserController implements IObserver {
     private IService service;
     private User user;
+
+    @FXML
+    private Button logoutBtn;
 
     public void init_controller(IService service, User user) {
         this.service = service;
@@ -27,5 +34,12 @@ public class UserController implements IObserver {
     @Override
     public void packageDeleted(Long id) throws Exception {
 
+    }
+
+    public void onLogout(ActionEvent actionEvent) throws Exception {
+        Stage stage = (Stage) this.logoutBtn.getScene().getWindow();
+        stage.close(); // Close the stage
+
+        this.service.logout(this.user, this);
     }
 }
